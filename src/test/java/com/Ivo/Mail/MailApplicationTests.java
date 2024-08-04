a69d58809386b054834016ac5b3ba87532e48e43
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-08-04 17:31:53 +0700

    Ivo

---
+++ src/test/java/com/Ivo/Mail/MailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ivo.Mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
